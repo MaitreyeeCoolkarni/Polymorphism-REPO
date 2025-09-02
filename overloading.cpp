a5b1d93d3f99ee43df441a0e835e7639fa615d6e
MaitreyeeCoolkarni <dunkul@gmail.com>
2025-09-02 15:19:40 +0530

    Create overloading.cpp

---
+++ overloading.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Add {
+private:
+    double sum;
+public:
+   
+    Add(int a, int b,int c) {
+        sum = a + b+c;
+        cout << "Sum (double) = " << sum << endl;
+    }
+
+
+    Add(int a, int b) {
+        sum = a + b;
+        cout << "Sum (int) = " << sum << endl;
+    }
+};
+    
+int main() {
+    Add A1(5900000, 60000000,10);  
+    Add A2(5, 10);                 
+    return 0;
+}
